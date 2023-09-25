b8a84f74e47dbe9cd57fe397e0166ae143a0c460
Your Name1~ <pacomarvinm@gmail.com>
2023-09-25 14:26:04 +0800

    sign innnn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,9 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         {user ? (
-            <SignOutButton><span className = "text-white cursor-pointer">Sign out</span></SignOutButton>
+            <SignOutButton><span className = "text-white cursor-pointer">Sign outttttt</span></SignOutButton>
           ) : (
-            <SignInButton><span className = "text-white cursor-pointer">Sign in</span></SignInButton>
+            <SignInButton><span className = "text-white cursor-pointer">Sign innnnn</span></SignInButton>
           )}
         </div>
       </main>
